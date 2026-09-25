3317836c0e22a40369246a928d4257bcd1dafa5b
Squishy <squishy6094@gmail.com>
2026-09-25 10:29:20 -0700

    remove debugging

--- behaviors.lua
+++ behaviors.lua
@@ -936,7 +936,6 @@ local function bhv_ghost_coin_loop(o)
     elseif o.oAction == HIDDEN_BLUE_COIN_ACT_CAUGHT then
         if o.oHealth <= 0 then
             local total, count = count_carrier_objects(m.marioObj, o)
-            djui_chat_message_create(tostring(count).."/"..total)
             if count == total then
                 spawn_mist_particles()
                 spawn_coin_spawner(o, 5)
